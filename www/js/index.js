2cb9166bf0f2107b3e1c07c7a0ec1a547f29544b
Jasper <jasper.huizing@gmail.com>
2015-01-15 22:04:38 +0100

    Revert "added google maps"
    This reverts commit 9dc7b47a1f17cb772f734bac2ad52b77079e4b5b.

--- www/js/index.js
+++ www/js/index.js
@@ -7,29 +7,12 @@ function onDeviceReady() {
     function disp(pos) {
         $('.lat-view').html(pos.coords.latitude);
         $('.long-view').html(pos.coords.longitude);
-        drawMap(pos.coords.latitude, pos.coords.longitude);
     }
 
     $('#getIt').click(function() {
         navigator.geolocation.getCurrentPosition(disp);
     });
+    
 
-    function drawMap(var lat, var long) {
-            var myLatlng = new google.maps.LatLng(lat, long);
-            var mapOptions = {
-                zoom: 4,
-                center: myLatlng
-            }
-            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                title: 'Hello World!'
-            });
-        }
-
-       
 }
-
-
