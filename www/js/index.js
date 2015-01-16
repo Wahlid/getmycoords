e51670e99fdceb3fefcadc352fb0a88a314eed06
Jasper <jasper.huizing@gmail.com>
2015-01-16 13:05:59 +0100

    get rid of maps

--- www/js/index.js
+++ www/js/index.js
@@ -5,13 +5,6 @@ function onDeviceReady() {
 
     alert("ready!");
 
-    var mapCanvas = document.getElementById('map-canvas');
-    var mapOptions = {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions)
 
     function disp(pos) {
         $('.lat-view').html(pos.coords.latitude);
